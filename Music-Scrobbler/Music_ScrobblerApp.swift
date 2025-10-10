055504551c700280da2497eb7f39aa4785c54207
NopXx <script.nop@gmail.com>
2025-10-10 14:16:31 +0700

    Initial Commit

---
+++ Music-Scrobbler/Music_ScrobblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_ScrobblerApp.swift
+//  Music-Scrobbler
+//
+//  Created by NopXx on 10/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct Music_ScrobblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
